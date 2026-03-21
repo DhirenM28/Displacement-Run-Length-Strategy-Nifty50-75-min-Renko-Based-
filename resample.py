956128a0c23ef4ffbf79f636c1ef5ffb194228ff
DhirenM28 <dhiren.mahibubani13@gmail.com>
2026-03-21 18:15:04 +0530

    Refactor data loading and resampling process
    Updated the script to load data from Kaggle and resample it.

--- resample.py
+++ resample.py
@@ -1,17 +1,33 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Mar 20 19:18:12 2026
 
-@author: Admin
-"""
+# Install dependencies as needed:
+# pip install kagglehub[pandas-datasets]
+# pip install pandas
+
+import kagglehub
+from kagglehub import KaggleDatasetAdapter
+
+# Set the path to the file you'd like to load
+file_path = "NIFTY 50_minute.csv"
+
+# Load the latest version
+df = kagglehub.load_dataset(
+  KaggleDatasetAdapter.PANDAS,
+  "debashis74017/nifty-50-minute-data",
+  file_path,
+  # Provide any additional arguments like 
+  # sql_query or pandas_kwargs. See the 
+  # documenation for more information:
+  # https://github.com/Kaggle/kagglehub/blob/main/README.md#kaggledatasetadapterpandas
+)
+
+print("First 5 records:", df.head())
 
 
 import pandas as pd
-df=pd.read_excel(r"1mindata.xlsx")
 df.set_index("date",inplace=True)
 df.index=pd.to_datetime(df.index)
 
-min_spot=df.resample("75min",origin="start_day",offset="9h15min").agg({
+resample_spot=df.resample("75min",origin="start_day",offset="9h15min").agg({
     'open': "first",
     'high': 'max',
     'low': 'min',
